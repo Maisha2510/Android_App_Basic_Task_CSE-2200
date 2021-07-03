e49a164eaa9e0c7ffb7f4665de6916d1be93c6a9
Maisha2510 <maisha2510@gmail.com>
2021-07-04 02:29:38 +0600

    CSE 2200 offline 1 (basic task)

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButtonID);
+        textView =  findViewById(R.id.textViewID);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Welcome to CSE 2200");
+            }
+        });
+    }
+}
